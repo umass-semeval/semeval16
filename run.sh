0a253d17352a0154a3857d8946a663fbb6bc1bdc
k8si <ksilvers@cs.umass.edu>
2015-11-16 19:46:30 -0500

    update run script

--- run.sh
+++ run.sh
@@ -5,12 +5,11 @@ trainData="$root/data/subtask-A/train.tsv"
 devData="$root/data/subtask-A/dev.tsv"
 testData="$root/data/subtask-A/test.tsv"
 
-modelType="logistic_regression"
 word2vec="/home/kate/research/word2vec/trunk/vectors.bin"
 
 python $root/semeval/main.py \
---train-file $trainData --dev-file $devData --test-file $testData \
---model-type $modelType \
---stopwords $root/lexica/stopwords.txt \
+--train-file $trainData \
+--dev-file $devData \
+--test-file $testData \
 --word2vec-model $word2vec
 
