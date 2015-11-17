d491aecc63ed09c3c694259c93cd0d9e21d1d063
k8si <ksilvers@cs.umass.edu>
2015-11-16 19:45:08 -0500

    update run script

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 root="/home/kate/Dropbox/F15/deep-learning/semeval16"
-data="$root/data/subtask-A/train.tsv"
+trainData="$root/data/subtask-A/train.tsv"
+devData="$root/data/subtask-A/dev.tsv"
+testData="$root/data/subtask-A/test.tsv"
 
-python $root/semeval/main.py --train-file $data --stopwords $root/lexica/stopwords.txt
+modelType="logistic_regression"
+word2vec="/home/kate/research/word2vec/trunk/vectors.bin"
+
+python $root/semeval/main.py \
+--train-file $trainData --dev-file $devData --test-file $testData \
+--model-type $modelType \
+--stopwords $root/lexica/stopwords.txt \
+--word2vec-model $word2vec
+
